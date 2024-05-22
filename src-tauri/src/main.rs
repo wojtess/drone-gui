f53166611fe8a15a56cabcde428682ff11ea5c64
wojtess <49391462+wojtess@users.noreply.github.com>
2024-05-22 17:07:54 +0200

    added some shiyt

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,11 +1,11 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use deku::{DekuContainerRead, DekuContainerWrite, DekuRead, DekuUpdate, DekuWrite};
 use serde::{Serialize, Deserialize};
-use tauri::{CustomMenuItem, Menu, Submenu, State, Manager, Window};
-use core::time;
+use tauri::{CustomMenuItem, Menu, Submenu, State};
 use std::fmt::Formatter;
 use std::sync::Mutex;
-use std::{thread, option, fmt::Debug, sync::Arc};
+use std::{fmt::Debug, sync::Arc};
 use std::time::Duration;
 use crossbeam_channel::{select, unbounded, tick, Sender, Receiver};
 
@@ -29,14 +29,15 @@ impl Debug for ControllsData {
          .finish()
     }
 }
-trait InPacket {
-    fn decode(&self);
+
+trait PacketOut {
+    fn encode(&self) -> Vec<u8>;
+
+    fn id(&self) -> u8;
 }
 
-trait OutPacket {
-    fn encode(&self);
-}
-
+#[derive(Debug, PartialEq, DekuRead, DekuWrite)]
+#[deku(endian = "little")]
 struct PacketIn0x01 {
     time: i32,
     throttle: u64,
@@ -44,10 +45,85 @@ struct PacketIn0x01 {
     roll: u64,
     yaw: u64,
 }
-impl InPacket for PacketIn0x01 {
-    fn decode(&self) {
-        todo!()
+
+#[derive(Debug, PartialEq, DekuRead, DekuWrite)]
+#[deku(endian = "little")]
+struct PacketOut0x01 {
+    throttle: u64,
+    pitch: u64,
+    roll: u64,
+    yaw: u64,
+}
+
+impl PacketOut for PacketOut0x01 {
+    fn encode(&self) -> Vec<u8> {
+        self.to_bytes().unwrap()
     }
+
+    fn id(&self) -> u8 {
+        0x01 
+    }
+}
+
+#[derive(Debug, PartialEq, DekuRead, DekuWrite)]
+#[deku(endian = "little")]
+struct PacketOut0x02 {
+    frequency: u32,
+    packet_3: bool,
+}
+
+impl PacketOut for PacketOut0x02 {
+    fn encode(&self) -> Vec<u8> {
+        self.to_bytes().unwrap()
+    }
+
+    fn id(&self) -> u8 {
+        0x02
+    }
+}
+
+#[derive(Debug, PartialEq, DekuRead, DekuWrite)]
+#[deku(endian = "little")]
+struct PacketOut0x03 {
+    throttle: u32,
+    pitch: u32,
+    roll: u32,
+    yaw: u32,
+}
+
+impl PacketOut for PacketOut0x03 {
+    fn encode(&self) -> Vec<u8> {
+        self.to_bytes().unwrap()
+    }
+
+    fn id(&self) -> u8 {
+        0x03
+    }
+}
+
+fn add_ieee_header(packet: Box<dyn PacketOut>) -> Vec<u8> {
+    let mut data = vec![
+        //radiotap header
+        0x00, // <-- radiotap version
+        0x00, // <-- padding
+        0x0b, 0x00, // <- radiotap header length
+        0x04, 0x0c, 0x00, 0x00, // <-- bitmap
+        0x6c, // <-- rate
+        0x0c, //<-- tx power
+        0x01, //<-- antenna
+        //ieee80211 header
+        0x48, 0x00, 0x00, 0x00,
+        0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+        0x13, 0x22, 0x33, 0x44, 0x55, 0x66,
+        0x13, 0x22, 0x33, 0x44, 0x55, 0x66,
+        0x00, 0x00,
+        //magic number
+        0x3C, 0x4A,
+        //finaly some data
+    ];
+    data.extend_from_slice(&[packet.id()]);
+    data.extend_from_slice(&*packet.encode());
+    data
 }
 
 fn thread_recieve(state: Arc<Mutex<ControllsData>>) {
@@ -58,7 +134,26 @@ fn thread_recieve(state: Arc<Mutex<ControllsData>>) {
                 let mut state = state.lock().unwrap();
                 if let Some(ref mut capture) = state.capture {
                     while let Ok(packet) = capture.next_packet() {
-
+                        let mut len = packet.header.caplen;
+                        let mut buf = packet.data.to_vec();
+                        if buf[0] != 0x48 {
+                            continue;
+                        }
+                        len -= 24;
+                        buf.drain(0..24);
+                        if len < 2/* maginc number */ + 1 {
+                            continue;
+                        }
+                        if buf[1] == 0x3C && buf[2] == 0x4A {
+                            buf.drain(0..2);
+                            len -= 2;
+                            match buf[3] {
+                                1 => {
+                                    let _ = PacketIn0x01::from_bytes((&buf[..], len as usize));
+                                },
+                                _ => {/* wrong id */},
+                            }
+                        }
                     }
                 }
             }
@@ -67,8 +162,7 @@ fn thread_recieve(state: Arc<Mutex<ControllsData>>) {
 
 }
 
-fn thread_80211(rx: Receiver<ChannelData>, app_hanlder: tauri::AppHandle, state: Arc<Mutex<ControllsData>>) {
-    let timer = tick(Duration::from_millis(10));
+fn thread_80211(rx: Receiver<ChannelData>, _: tauri::AppHandle, state: Arc<Mutex<ControllsData>>, packets: Receiver<Box<dyn PacketOut + Send>>) {
     loop {
         select! {
             recv(rx) -> input => {
@@ -114,44 +208,14 @@ fn thread_80211(rx: Receiver<ChannelData>, app_hanlder: tauri::AppHandle, state:
                     }
                 }
             },
-            recv(timer) -> _ => {
-                let mut state0 = state.lock().unwrap();
-                //WAZNE USTAWIC W USTAWIENIACH INTERFACE INACZEJ NIE ZADZIALA
-                let mut data = vec![
-                    //radiotap header
-                    0x00, // <-- radiotap version
-                    0x00, // <-- padding
-                    0x0b, 0x00, // <- radiotap header length
-                    0x04, 0x0c, 0x00, 0x00, // <-- bitmap
-                    0x6c, // <-- rate
-                    0x0c, //<-- tx power
-                    0x01, //<-- antenna
-                    //ieee80211 header
-                    0x48, 0x00, 0x00, 0x00,
-                    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-                    0x13, 0x22, 0x33, 0x44, 0x55, 0x66,
-                    0x13, 0x22, 0x33, 0x44, 0x55, 0x66,
-                    0x00, 0x00,
-                    //magic number
-                    0x3C, 0x4A,
-                    //finaly some data
-                    0x01,//packet id
-                ];
-                {
-                    data.extend_from_slice(&state0.throttle.to_be_bytes());
-                    data.extend_from_slice(&state0.pitch.to_be_bytes());
-                    data.extend_from_slice(&state0.roll.to_be_bytes());
-                    data.extend_from_slice(&state0.yaw.to_be_bytes());
-                }
-                if let Some(ref mut capture) = state0.capture {
-                    // data.extend_from_slice(&((state.throttle*1000.0)as i64).to_be_bytes());
-                    // data.extend_from_slice(&((state.pitch*1000.0)as i64).to_be_bytes());
-                    // data.extend_from_slice(&((state.roll*1000.0)as i64).to_be_bytes());
-                    // data.extend_from_slice(&((state.yaw*1000.0)as i64).to_be_bytes());
-                    // send_msg("sending packet".to_string()).expect("Failed to call send_variable");
-                    if let Err(err) = capture.sendpacket(data) {
-                        //add err support
-                        // send_msg(("error while sending packet: {:?}", err).to_string()).expect("Failed to call send_variable");
+            recv(packets) -> input => {
+                if let Ok(packet) = input {
+                    let mut state0 = state.lock().unwrap();
+                    let data = add_ieee_header(packet);
+                    if let Some(ref mut capture) = state0.capture {
+                        if let Err(_) = capture.sendpacket(data) {
+                            //TODO: add error
+                        }
                     }
                 }
             }
@@ -202,7 +266,7 @@ fn get_devices() -> Vec<Device> {
 #[tauri::command]
 fn set_device(state: State<Sender<ChannelData>>, device: Device) {
     //send_msg(state, "set_device".to_string());
-    state.send(ChannelData::UseDevice(device));
+    let _ = state.send(ChannelData::UseDevice(device));
 }
 
 #[derive(Clone, serde::Serialize)]
@@ -213,28 +277,54 @@ struct Payload {
 fn main() {
     
     let (tx, rx) = unbounded();
+    let (tx_packets, rx_packets) = unbounded::<Box<(dyn PacketOut + Send + 'static)>>();
 
     
     let state = Arc::new(Mutex::new(ControllsData::default()));
     let state0 = Arc::clone(&state);
-    let state1 = Arc::clone(&state);
+    
 
     let settings = CustomMenuItem::new("settings".to_string(), "Settings");
     let file_submenu = Submenu::new("File", Menu::new().add_item(settings));
     let menu = Menu::new()
         .add_submenu(file_submenu);
 
-    tauri::async_runtime::spawn(async move {
-        thread_recieve(state1);
-    });
+    {
+        let state1 = Arc::clone(&state);
+        tauri::async_runtime::spawn(async move {
+            thread_recieve(state1);
+        });
+    }
+   
+    {
+        let state2 = Arc::clone(&state);
+        tauri::async_runtime::spawn(async move {
+            let timer = tick(Duration::from_millis(10));
+            loop {
+                select! {
+                    recv(timer) -> _ => {
+                        let state = state2.lock().unwrap();
+                        let packet = PacketOut0x03 {
+                            pitch: (state.pitch * 4294967295.0 / 100.0) as u32,
+                            roll: (state.roll * 4294967295.0 / 100.0) as u32,
+                            throttle: (state.throttle * 4294967295.0 / 100.0) as u32,
+                            yaw: (state.yaw * 4294967295.0 / 100.0) as u32
+                        };
+                        let _ = tx_packets.send(Box::new(packet));
+                    }
+                }
+            }
+        });
+    }
+    
 
-    let app = tauri::Builder::default()
+    let _app = tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![set_controlls, get_devices, set_device])
         .menu(menu)
         .setup(|app| {
             let app_hanlder=app.handle();
             tauri::async_runtime::spawn(async move {
-                thread_80211(rx, app_hanlder, state0);
+                thread_80211(rx, app_hanlder, state0, rx_packets);
             });
             Ok(())
         })
